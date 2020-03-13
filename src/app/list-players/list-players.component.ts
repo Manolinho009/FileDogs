a5817a4c653d75e86e38577e6105bb37e6b02664
manolinho009 <thiagohenrique16@gmail.com>
2020-03-12 23:10:17 -0300

    Revert "Mudanças no database"
    This reverts commit fb71a21ecd97925fd22330783b3d1ac917e7b497.

--- src/app/list-players/list-players.component.ts
+++ src/app/list-players/list-players.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from './player';
 import { PlayerService } from './player.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-players',
@@ -14,25 +13,17 @@ export class ListPlayersComponent implements OnInit {
   player : Player
   players : Observable<any>
   nome : string
-  key : string = ''
 
   constructor(private playerService : PlayerService) { }
 
   ngOnInit() {
-    this.player = new Player()
     this.players = this.playerService.getAll()
   }
 
-  // login(){
-  //   this.player = new Player()
-  //   this.player.nome = "aa"//this.nome
-  //   this.playerService.insert(this.player)
-  // }
-
   login(){
-    this.playerService.insert(this.player)
-    console.log("pipop"+this.player.nome)
     this.player = new Player()
+    this.player.nome = this.nome
+    this.playerService.insert(this.player)
   }
-  
+
 }
